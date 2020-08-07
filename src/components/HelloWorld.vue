746ec758fb03f05b9d9d8a6031cf0b8c67291f92
Anatoly Tarnavsky <anatolyt@gmail.com>
2020-08-08 01:57:48 +0300

    Book can be opened from a file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button @click="expose">Expose</button>
+    <div style="margin-top: auto; margin-bottom: auto; margin-left: 10px;"> <!-- https://stackoverflow.com/a/54677618/947111 -->
+      <button @click="$refs.inputUpload.click()">Upload Subtitles</button>
+      <input v-show="false" ref="inputUpload" type="file" @change="createBook">
+    </div>
+    <button @click="next">Next</button>
+    <div id="book-area"></div>
   </div>
 </template>
 
 <script>
+import ePub from 'epubjs';
 export default {
   name: 'HelloWorld',
+  data() {
+    return {
+      rendition: null,
+      book: null,
+      displayed: null
+    }
+  },
+  methods: {
+    expose () {
+      console.log('rendition', this.rendition);
+      console.log('book', this.book);
+      console.log('displayed', this.displayed);
+    },
+    next() {
+      this.rendition.next();
+    },
+    async createBook (event) {
+      const file = await this.uploadFile(event);
+      this.book = ePub();
+      this.book.open(file, "binary");
+      this.rendition = this.book.renderTo("book-area", {width: 600, height: 800});
+      this.displayed = this.rendition.display();
+    },
+    uploadFile (event) {
+      return new Promise((resolve, reject) => {
+        const files = event.target.files || event.dataTransfer.files;
+        if (!files.length) {
+          return;
+        }
+        const file = files[0];
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const result = e.target.result;
+          const base64 = result.split(",")[1];
+          const string = atob(base64);
+          return resolve(string);
+        };
+
+        reader.onerror = (error) => {
+          return reject(error);
+        }
+
+        reader.readAsDataURL(file);
+      })
+    }
+  },
+  mounted() {
+
+  },
   props: {
     msg: String
   }
@@ -41,18 +72,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+console.log(window.ipcRenderer)
+
+window.ipcRenderer.on('hello', (event, arg) => {
+  // Get the current Vue instance (i.e. which component/route is currently active)
+  // let component = router.currentRoute.matched[0].instances.default
+  //
+  // component.someReactiveData = 'Received message from main process
+  console.log(event, arg);
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
